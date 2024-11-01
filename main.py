21389a7006c592a5ffaea1aa64d78eeae5604f6d
Mosiv <mosiv@foxmail.com>
2024-11-01 12:56:34 +0800

    update .gitignore

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ _  __/   / /_/ /_  /   / /_/ /     _  /_/ // /_/ /  /_/ // /_/ / /__ __>  <
     parser.add_argument(
         '--version', '-v',
         action='version',
-        version='%(prog)s 0.2.0',
+        version='%(prog)s 0.3.0',
         help='显示程序版本信息'
     )
 
